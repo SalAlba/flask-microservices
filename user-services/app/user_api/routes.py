dd04a8c2c5ff1365ed212f1e8db50210d6c3a39a
Salem <saljan93@live.com>
2020-09-23 17:25:38 +0200

    add new endpoints to user services

--- user-services/app/user_api/routes.py
+++ user-services/app/user_api/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify, render_template
+from flask import jsonify, request, render_template
 # from flask_login import login_required
 
 from . import user_blueprint as current_blueprint
@@ -23,9 +23,9 @@ def index():
 @current_blueprint.route('/users', methods=['GET'])
 # @login_required
 # @loge # TODO ...
-def get_all_users():
+def get_users():
     data = dict(
-        data=[row.to_json() for row in User.query.all()],
+        result=[row.to_json() for row in User.query.all()],
         message={
             'info': 'success',
             'code': 201
@@ -34,3 +34,61 @@ def get_all_users():
     return data
 
 
+@current_blueprint.route('/user/id/<_id>', methods=['GET'])
+# @login_required
+# @loge # TODO ...
+def get_user_by_id(_id):
+
+    item = User.query.filter_by(username=username).first()
+    if not item:
+        response = jsonify(dict(
+            result=item,
+            message={
+                'info': 'success',
+                'code': 201
+            },
+        ))
+    else:
+        response = jsonify(dict(
+            message={
+                'info': 'success',
+                'code': 201
+            }
+            )), 404
+
+    return response
+
+
+@user_api_blueprint.route('/api/user/create', methods=['POST'])
+# @login_required
+# @loge # TODO ...
+def create_user():
+    first_name = request.get('first_name')
+    last_name = request.get('last_name')
+    email = request.get('email')
+    username = request.get('username')
+    password = sha256_crypt.hash((str(request.get('password'))))
+
+    # TODO move to controller ..
+    user = User()
+    user.email = email
+    user.first_name = first_name
+    user.last_name = last_name
+    user.password = password
+    user.username = username
+    user.authenticated = True
+    user.active = True
+
+    # ...
+    db.session.add(user)
+    db.session.commit()
+
+    response = dict(
+        result=user.to_json(),
+        message={
+            'info': 'User added',
+            'code': 201
+        },
+    )
+
+    return jsonify(response)
