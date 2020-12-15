3ef520054186ff5c948d83af5cc4d91e39ef8d34
salalba <saljan93@live.com>
2020-12-15 19:30:42 +0100

    fix my simple bug #1

--- user-services/run_app.py
+++ user-services/run_app.py
@@ -1,7 +1,7 @@
 from app import create_app
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     app = create_app()
     # app.run(host='', port=app.config['PORT'])
     app.run(host='127.0.0.1', port='2020', debug=True)
