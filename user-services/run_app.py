d847f072eca7bddc42605d2d4e0f17984097cccf
salalba <saljan93@live.com>
2020-12-16 21:47:15 +0100

    test run actions for deploy

--- user-services/run_app.py
+++ user-services/run_app.py
@@ -4,4 +4,5 @@ from app import create_app
 if __name__ == '__main__':
     app = create_app()
     # app.run(host='', port=app.config['PORT'])
-    app.run(host='127.0.0.1', port='2020', debug=True)
+    # app.run(host='127.0.0.1', port='2020', debug=True)
+    app.run(host='127.0.0.1', port='2020')
